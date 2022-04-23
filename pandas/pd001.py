ac048d0c3d43abc8a0a799963171c2f7e134ff89
Akira <2273076505@qq.com>
2022-04-23 13:29:50 +0800

    new

---
+++ pandas/pd001.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+import pandas as pd
+
+path = 'F:\document\OneDrive - 南京农业大学\My_codes\python\study_pandas\pd001.csv'
+# data = pd.DataFrame({'1':[1,2,3,4,5,6,7,8,9],'2':['11','22','33','44','55','66','77','88','99']})
+# data = data.set_index('1')
+# data.to_csv(path)
+
+data = pd.read_table(path,sep=',')
+print(data)
+
+print('新建完成')
---
+++ re/re.py
@@ -0,0 +1,7 @@
+# import re
+
+# a = 'group__2273076505'
+# ids = re.findall('\d{1,11}',a)[::-1]
+# b = ids[0]
+# c = ids[1]
+# print(ids,b,c)
